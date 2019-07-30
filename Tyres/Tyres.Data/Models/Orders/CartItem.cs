e7b48c9fc4000a70174fa6a969d381afedc3f0bb
nikola <nikolavodeni4arov@gmail.com>
2019-07-30 11:24:04 +0300

    Models for order.

---
+++ Tyres/Tyres.Data/Models/Orders/CartItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tyres.Data.Models.Orders
+{
+    public class CartItem
+    {
+        public string ItemId { get; set; }
+
+        public string CartId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
